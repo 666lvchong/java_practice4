42e9ad67a8915b434355ec30fbbf619a9e8c1fe0
15984129672fanyuxin <36066081+15984129672fanyuxin@users.noreply.github.com>
2018-03-08 16:45:08 +0800

    删除

--- target/Team4-1.0-SNAPSHOT/js/user_info.js
+++
@@ -1,9 +0,0 @@
-
-//删除事件
-function del(obj){
-    var trId=obj.parentNode.parentNode.id;
-    var trObj=document.getElementById(trId);
-    document.getElementById("table_body_id").removeChild(trObj);
-}
-
-
